6fa25996572f6c09f338da6c910277b9a163107b
Tsegaye Tenkir <tsegaye@gmail.com>
2021-01-11 22:07:28 -0600

    The First Commited Fresh Spring Boot Product Exchange Rate

---
+++ src/main/java/com/exchangerate/product/dashboard/ProductexchangerateAppApplication.java
@@ -0,0 +1,13 @@
+package com.exchangerate.product.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductexchangerateAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductexchangerateAppApplication.class, args);
+	}
+
+}
